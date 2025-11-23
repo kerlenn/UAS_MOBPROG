92ce61ddfcf6b9fc333f4f89625ce150a6436810
R21-2000 <rakhafian9010@gmail.com>
2025-11-23 20:49:30 +0700

    Feat: setup rute navigation entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'screens/home/home.dart';
 import 'screens/products/products_screen.dart';
-main
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
